8e416410ef17906dd75cdfb6270a674b07ee7e49
FernandoGM2701 <fernandogm2701@gmail.com>
2019-09-27 17:52:42 -0500

    Generar PDF - 2

--- app/src/main/java/com/example/generarpdf/TemplatePDF.java
+++ app/src/main/java/com/example/generarpdf/TemplatePDF.java
@@ -5,18 +5,32 @@ import android.os.Environment;
 import android.util.Log;
 
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 
 public class TemplatePDF {
 
     private Context context;
     private File pdfFile;
-    private Document documemt;
+    private Document document;
+    private Paragraph paragraph;
+    private Font fTitle= new Font(Font.FontFamily.TIMES_ROMAN, 20, Font.BOLD);
+    private Font fSubTitle = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
+    private Font fText = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+    private Font fHighText = new Font(Font.FontFamily.TIMES_ROMAN, 15, Font.BOLD, BaseColor.RED);
+
 
     private PdfWriter pdfWriter;
     public TemplatePDF(){
@@ -34,9 +48,9 @@ public class TemplatePDF {
     public void openDocument(){
         createFile();
         try {
-        documemt = new Document(PageSize.A4);
-        pdfWriter = PdfWriter.getInstance(documemt, new FileOutputStream(pdfFile));
-        documemt.open();
+        document = new Document(PageSize.A4);
+        pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(pdfFile));
+        document.open();
 
         }catch (Exception e){
             Log.e("openDocument",e.toString());
@@ -44,17 +58,74 @@ public class TemplatePDF {
     }
 
     public void closeDocument(){
-        documemt.close();
+        document.close();
     }
 
     public void addMetaData(String title, String subject, String author){
-        documemt.addTitle(title);
-        documemt.addSubject(subject);
-        documemt.addAuthor(author);
+        document.addTitle(title);
+        document.addSubject(subject);
+        document.addAuthor(author);
     }
 
     public void addTitles(String title, String subTitle, String date){
-        
+        try {
+            paragraph = new Paragraph();
+            addChildP(new Paragraph(title, fTitle));
+            addChildP(new Paragraph(title, fSubTitle));
+            addChildP(new Paragraph("Generado: " + date, fHighText));
+            paragraph.setSpacingAfter(30);
+            document.add(paragraph);
+        }catch (Exception e) {
+            Log.e("addTitles", e.toString());
+        }
+    }
+
+    private void addChildP(Paragraph childParagrah){
+        childParagrah.setAlignment(Element.ALIGN_CENTER);
+        paragraph.add(childParagrah);
+    }
+
+    public void addParagraph(String text){
+        try {
+            paragraph = new Paragraph(text, fText);
+            paragraph.setSpacingAfter(5);
+            paragraph.setSpacingBefore(5);
+            document.add(paragraph);
+        }catch (Exception e){
+            Log.e("addParagrah", e.toString());
+        }
+    }
+
+    public void createTable(String[]header, ArrayList<String[]>clients){
+        try {
+            paragraph = new Paragraph();
+            paragraph.setFont(fText);
+            PdfPTable pdfPTable = new PdfPTable(header.length);
+            pdfPTable.setWidthPercentage(100);
+            PdfPCell pdfPCell;
+            int indexC = 0;
+
+            while (indexC < header.length) {
+                pdfPCell = new PdfPCell(new Phrase(header[indexC++], fSubTitle));
+                pdfPCell.setHorizontalAlignment(Element.ALIGN_BASELINE);
+                pdfPCell.setBackgroundColor(BaseColor.GREEN);
+                pdfPTable.addCell(pdfPCell);
+            }
+
+            for (int indexR = 0; indexR < clients.size(); indexR++) {
+                String[] row = clients.get(indexR);
+                for (indexC = 0; indexC < clients.size(); indexC++) {
+                    pdfPCell = new PdfPCell(new Phrase(row[indexC]));
+                    pdfPCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    pdfPCell.setFixedHeight(40);
+                    pdfPTable.addCell(pdfPCell);
+                }
+            }
+            paragraph.add(pdfPTable);
+            document.add(paragraph);
+        }catch (Exception e){
+            Log.e("addParagraph", e.toString());
+        }
     }
 
 
